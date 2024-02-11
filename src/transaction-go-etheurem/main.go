d4ed08f20b1061ed110bc29e442ed8df9e421847
Alexandru <alexandruandrei659.aa@gmail.com>
2024-02-11 16:11:22 +0100

    update main.go

--- src/transaction-go-etheurem/main.go
+++ src/transaction-go-etheurem/main.go
@@ -1,29 +1,43 @@
 package main
+
 import (
-	"fmt"
-	"log"	
 	"context"
-    "log"
-	
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
-    "github.com/ethereum/go-ethereum/common"
-//    "github.com/ethereum/go-ethereum/crypto"
 )
 
-var(
-	url = "https://sepolia.infura.io/v3/7f0d5493f09941789897ae6ea75788a0"
+var (
+	url  = "https://kovan.infura.io/v3/0c7b3f204f37416388610fb274b0452c"
+	murl = "https://mainnet.infura.io/v3/0c7b3f204f37416388610fb274b0452c"
 )
 
-func main(){
-	
+func main() {
+	// ks := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.StandardScryptP)
+	// _, err := ks.NewAccount("password")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// _, err = ks.NewAccount("password")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// "1f7ecea2fa83cc4a7de969f11d16a40edf9023d7"
+	// "1e41ca1ccfc06597525c966a986b35a09e22358d"
+
 	client, err := ethclient.Dial(url)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer client.Close()
-	a1 := common.HexToAddress("c4e5a877a603d740d1baf8d35206707160dc8cce")
-	a2 := common.HexToAddress("ada64c03bb8ecb118f71585df729b4ae1b1eb2ef")
-	
+	a1 := common.HexToAddress("c393967d7b4b7fd02e697d13085d645c9412af11")
+	a2 := common.HexToAddress("1e41ca1ccfc06597525c966a986b35a09e22358d")
 
 	b1, err := client.BalanceAt(context.Background(), a1, nil)
 	if err != nil {
@@ -35,8 +49,8 @@ func main(){
 		log.Fatal(err)
 	}
 
-	fmt.Println("Balance 1: ", b1)
-	fmt.Println("Balance 2: ", b2)
+	fmt.Println("Balance 1:", b1)
+	fmt.Println("Balance 2:", b2)
 	nonce, err := client.PendingNonceAt(context.Background(), a1)
 	if err != nil {
 		log.Fatal(err)
@@ -47,21 +61,23 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	tx := types.NewTransaction(nonce, a2, amount, 21000, gasPrice, nil)
-	
 	chainID, err := client.NetworkID(context.Background())
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	b, err := ioutil.ReadFile("wallet/UTC--2024-02-11T11-18-03.210022925Z--c4e5a877a603d740d1baf8d35206707160dc8cce")
+	b, err := ioutil.ReadFile("wallet/UTC--2021-05-24T16-47-26.459903259Z--c393967d7b4b7fd02e697d13085d645c9412af11")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	keystore.DecryptKey(b, "password")
-	tx, err := types.SignTx(tx, types.NewEIP155Signer(chainID), key.PrivateKey)
+	key, err := keystore.DecryptKey(b, "password")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	tx, err = types.SignTx(tx, types.NewEIP155Signer(chainID), key.PrivateKey)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,6 +86,5 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	fmt.Printf("tx sent: %s", tx.Hash().Hex())
 }
