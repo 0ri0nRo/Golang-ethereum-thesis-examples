5c50c5764b5e2cdf9908b020df47d9d4d967eef9
Alexandru <alexandruandrei659.aa@gmail.com>
2024-02-11 12:32:00 +0100

    update

--- init/get-ether-test/main.go
+++ init/get-ether-test/main.go
@@ -1,4 +1,9 @@
-import "github.com/ethereum/go-ethereum/ethclient"
+import (
+	"fmt"
+	"log"	
+	"github.com/ethereum/go-ethereum/ethclient"
+
+)
 
 var(
 	url = "https://goerli.infura.io/v3/7f0d5493f09941789897ae6ea75788a0"
@@ -14,5 +19,19 @@ func main(){
 	defer cliend.Close()
 	a1 := common.HexToAddress("c4e5a877a603d740d1baf8d35206707160dc8cce")
 	a2 := common.HexToAddress("ada64c03bb8ecb118f71585df729b4ae1b1eb2ef")
-	client.BalanceAt(context.Background(), a1, nil)
+	
+
+	b1, err := client.BalanceAt(context.Background(), a1, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b2, err := client.BalanceAt(context.Background(), a2, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Balance 1: ", b1)
+	fmt.Println("Balance 2: ", b2)
+
 }
