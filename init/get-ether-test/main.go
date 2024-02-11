b544f126553a3ed2f000e92fba4d7010592099cc
Alexandru <alexandruandrei659.aa@gmail.com>
2024-02-11 12:21:12 +0100

    update

--- init/get-ether-test/main.go
+++ init/get-ether-test/main.go
@@ -1,22 +1,4 @@
 package main
-
-import (
-	"log"
-
-	"github.com/ethereum/go-ethereum/accounts/keystore"
-
-)
-
 func main(){
-	ks := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.StandardScryptP)
-
-	_, err := ks.NewAccount("password")
-	if err != nil{
-		log.Fatal(err)
-	}
-
-	_, err = ks.NewAccount("password")
-	if err != nil{
-		log.Fatal(err)
-	}
+	
 }
