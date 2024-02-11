9049ae3df7bacb1b120519638dbbe07938ec1961
Alexandru <alexandruandrei659.aa@gmail.com>
2024-02-11 12:27:44 +0100

    update

--- init/get-ether-test/main.go
+++ init/get-ether-test/main.go
@@ -1,11 +1,16 @@
 import "github.com/ethereum/go-ethereum/ethclient"
 
 var(
-	url = ""
+	url = "https://goerli.infura.io/v3/7f0d5493f09941789897ae6ea75788a0"
 )
 
 package main
 func main(){
-	ethclient.Dial("")
+	
+	client, err := ethclient.Dial("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer cliend.Close()
 	
 }
