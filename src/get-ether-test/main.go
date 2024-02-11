68106a1283f350740959145f941c732e63ec3602
Alexandru <alexandruandrei659.aa@gmail.com>
2024-02-11 12:46:20 +0100

    update eth

--- src/get-ether-test/main.go
+++ src/get-ether-test/main.go
@@ -3,15 +3,15 @@ import (
 	"fmt"
 	"log"	
 	"context"
-    "log"
+    //"log"
 	
 	"github.com/ethereum/go-ethereum/ethclient"
     "github.com/ethereum/go-ethereum/common"
-    "github.com/ethereum/go-ethereum/crypto"
+//    "github.com/ethereum/go-ethereum/crypto"
 )
 
 var(
-	url = "https://goerli.infura.io/v3/7f0d5493f09941789897ae6ea75788a0"
+	url = "https://sepolia.infura.io/v3/7f0d5493f09941789897ae6ea75788a0"
 )
 
 func main(){
@@ -20,7 +20,7 @@ func main(){
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer cliend.Close()
+	defer client.Close()
 	a1 := common.HexToAddress("c4e5a877a603d740d1baf8d35206707160dc8cce")
 	a2 := common.HexToAddress("ada64c03bb8ecb118f71585df729b4ae1b1eb2ef")
 	
