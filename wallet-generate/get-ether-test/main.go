c8779af523ef00ae2f3dfec0f506de749bba319b
Alexandru <alexandruandrei659.aa@gmail.com>
2024-02-11 12:10:57 +0100

    update

---
+++ wallet-generate/get-ether-test/main.go
@@ -0,0 +1,20 @@
+import (
+	"log"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+
+)
+
+func main(){
+	ks := keystore.NewKeyStore("./wallet", keystore.StandardScryptN, keystore.StandardScryptP)
+
+	_, err := ks.NewAccount("password")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	_, err = ks.NewAccount("password")
+	if err != nil{
+		log.Fatal(err)
+	}
+}
